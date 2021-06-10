a0c52a981d2e225762a3f55ba69c1ed0769a0f4b
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2021-06-10 17:08:23 +0530

    added CSES task - Edit Distance
    https://cses.fi/problemset/task/1639

---
+++ edit_distance.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from io import BytesIO, IOBase
+
+ONLINE_JUDGE = 1
+
+# ___________________________________________________ #
+
+
+def edit_dis(s1, s2):
+    n, m = len(s1), len(s2)
+    dp = [[0 for i in range(m + 1)] for i in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = i
+    for j in range(m + 1):
+        dp[0][j] = j
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+    return dp[n][m]
+
+
+def main():
+    s1 = input()
+    s2 = input()
+    print(edit_dis(s1, s2))
+
+
+# ___________________________________________________ #
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+
+def input():
+    return sys.stdin.readline().rstrip("\r\n")
+
+
+if __name__ == "__main__":
+    if not ONLINE_JUDGE:
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+    main()
