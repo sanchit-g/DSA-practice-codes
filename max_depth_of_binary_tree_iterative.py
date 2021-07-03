283270af8bcf9b8cffbd4fd8e4bb7c473ea6c605
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2021-07-03 13:02:50 +0530

    added LC ques 104
    https://leetcode.com/problems/maximum-depth-of-binary-tree/

---
+++ max_depth_of_binary_tree_iterative.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        queue = deque([root])
+        depth = 0
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+        return depth
