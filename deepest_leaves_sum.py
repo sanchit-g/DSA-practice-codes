03151101ab61e12d5df54d0ef1267970cd362e12
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2021-07-10 10:16:21 +0530

    added LC ques 1302
    https://leetcode.com/problems/deepest-leaves-sum/

---
+++ deepest_leaves_sum.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        # BFS
+        queue = deque([root])
+        ans = 0
+        while queue:
+            ans = 0
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                ans += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return ans
