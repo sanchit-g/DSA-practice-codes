55e938c5edddb534810411a2d764bd24eb9ffb65
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2021-09-12 10:28:06 +0530

    added LC ques 1007
    https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/

---
+++ min_domino_rotations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        same, countA, countB = Counter(), Counter(A), Counter(B)
+        for a, b in zip(A, B):
+            if a == b:
+                same[a] += 1
+        for i in range(1, 7):
+            if countA[i] + countB[i] - same[i] == len(A):
+                return min(countA[i], countB[i]) - same[i]        
+        return -1
