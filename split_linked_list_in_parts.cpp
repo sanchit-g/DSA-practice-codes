f1bb2839acc8404158174dd04185160414f894b4
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2022-04-04 22:49:24 +0530

    added LC ques 725
    https://leetcode.com/problems/split-linked-list-in-parts/

---
+++ split_linked_list_in_parts.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    int length(ListNode* head) {
+        int ans = 0;
+        while (head) {
+            ans++;
+            head = head -> next;
+        }
+        return ans;
+    }
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+
+        int n = length(root);
+        ListNode* head = root;
+        int parts = n / k;
+        int len = n / k;
+        int elementsRemaining = n % k;
+        vector<ListNode*> ans;
+
+        if (parts == 0) {
+            // every segment will have at max 1 element
+            for (int i = 0; i < k; i++) {
+                if (head) {
+                    ListNode* temp = head -> next;
+                    head -> next = NULL;
+                    ans.push_back(head);
+                    head = temp;
+                }
+                else
+                    ans.push_back(NULL);
+            }
+        }
+
+        else {
+            int count = 1;
+            while (head) {
+                ans.push_back(head);
+                int x = elementsRemaining > 0 ? 1 : 0;
+                elementsRemaining--;
+                for (int i = 1; i < len + x; i++) {
+                    head = head -> next;
+                }
+                ListNode* temp = head -> next;
+                head -> next = NULL;
+                head = temp;
+                count++;
+                if (count == k) {
+                    ans.push_back(head);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
