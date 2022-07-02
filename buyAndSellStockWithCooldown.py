580b6a7930c2001be9dd918aa17430825f5e0a6f
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2022-07-02 13:27:03 +0530

    added LC ques 309
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

---
+++ buyAndSellStockWithCooldown.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        @lru_cache(maxsize=None)
+        def solve(idx, buy):
+            # base case
+            if idx >= len(prices):
+                return 0
+            
+            if buy == 1:
+                profit = max((-prices[idx] + solve(idx+1, 0)), solve(idx+1, 1))
+            else:
+                profit = max((prices[idx] + solve(idx+2, 1)), solve(idx+1, 0))
+            
+            return profit
+        
+        return solve(0, 1)
